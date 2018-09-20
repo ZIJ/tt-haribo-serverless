6a4605436c0d48f8cfc66647b6e8abe329cda553
Igor Zalutski <igor.zalutski@tictrac.com>
2018-09-20 12:59:09 +0100

    Move to Vue.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 let database = firebase.database();
 
 firebase.database().ref('/test-message').once('value').then(function(snapshot) {
-    let value = snapshot.val();
-    document.querySelector("#target").innerHTML = value.toString();
+    let test_data = snapshot.val();
+
+    var app = new Vue({
+        el: '#app',
+        data: {
+          message: 'Go Team Haribo!',
+          test_data,
+          articles: [1,2,3]
+        }
+      })
 });
