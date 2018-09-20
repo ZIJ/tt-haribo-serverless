a5788e3b4a4ef8cbf4c29b2a41c80c822a462cd1
Igor Zalutski <igor.zalutski@tictrac.com>
2018-09-20 14:46:49 +0100

    Switch to VueFire

--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
 let database = firebase.database();
+let articleRef = database.ref('/articles');
 
-firebase.database().ref('/articles').once('value').then(function(snapshot) {
-    let articles = snapshot.val();
-    console.log(articles);
-
-    var app = new Vue({
-        el: '#app',
-        data: {
-          message: 'Go Team Haribo!',
-          articles,
-        }
-      })
+let app = new Vue({
+    el: '#app',
+    firebase: {
+        articles: articleRef
+    },
+    data: {
+      message: 'Go Team Haribo!',
+    }
 });
