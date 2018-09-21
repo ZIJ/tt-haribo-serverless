66e58b5d08d2508458d72548c70d4479a9c4dae0
Igor Zalutski <igor.zalutski@tictrac.com>
2018-09-21 13:14:18 +0100

    Add sendVoteNotification function

--- functions/index.js
+++ functions/index.js
@@ -7,3 +7,10 @@ const functions = require('firebase-functions');
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
+
+exports.sendVoteNotification = functions.database.ref('/article_votes/{voteId}')
+    .onWrite((change, context) => {
+        admin.messaging().send("Vote added").then((response) => {
+            console.log(response);
+        })
+});
