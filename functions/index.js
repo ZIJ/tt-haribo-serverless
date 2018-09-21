c0ccc4b6f73f2b0d5a042304aafe8e85209daaef
Igor Zalutski <igor.zalutski@tictrac.com>
2018-09-21 14:01:27 +0100

    Swithc to legacy Messaging API

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,10 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
+const deviceTokens = [
+    'da3iOsvPgGw:APA91bFQHAu-Q5iXJ4m3HIDXyhd-R2NIG3mmyYK8wG9SC25np3PSV4QUPWBdm6QNYxe3-cg54v4Uo6xtP_xMeFQMXidgVBrcQvy7m9xZQelBSg_TwZ309BN5IuqJszLFI-x1nrJO08LX'
+];
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
@@ -12,14 +16,15 @@ exports.sendVoteNotification = functions.database.ref('/article_votes/{voteId}')
         let messagePayload = {
             notification: {
                 title: 'True or False?',
-                body: 'What do you think…',
-                content_available : true,
-                mutable_content: true,
-                priority : 'high'
-            },
-            topic: 'all'
-        }
-        admin.messaging().send(messagePayload).then((response) => {
+                body: 'What do you think…'
+            }
+        };
+        let messageOptions = {
+            content_available : true,
+            mutable_content: true,
+            priority : 'high'
+        };
+        admin.messaging().sendToDevice(deviceTokens, messagePayload, messageOptions).then((response) => {
             console.log(response);
         })
 });
